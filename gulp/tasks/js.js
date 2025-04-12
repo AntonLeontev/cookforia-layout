72f96ceabaa8d00f1691dedefd16428ed1187ad3
aleksei.kazulin <Kazulin.job@gmail.com>
2025-04-12 20:03:36 +0300

    Сделано: Попап с формой Callback; страница рецептов; страница отдельного рецепта

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,8 +1,10 @@
 import concat from "gulp-concat";
+import terser from "gulp-terser";
 
 export const js = () => {
-    return app.gulp.src(app.path.src.js, {sourcemaps: app.isDev })
+    return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
         .pipe(concat('app.min.js'))
+        .pipe(terser())
         .pipe(app.gulp.dest(app.path.build.js))
         .pipe(app.plugins.browsersync.stream());
 }
